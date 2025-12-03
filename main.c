b298ad79ad2b6e2d5f0573421a1084836ca93428
jaehyunoo <yujunwoo9197@naver.com>
2025-12-03 23:27:16 +0900

    Revert "yjh merge into dev"
    This reverts commit 4ddf533f4644a3d75fe8db9b1a758e03950822fe, reversing
    changes made to d45cd0b32bde5af06dbbe57198735def7d3ff4bd.

--- main.c
+++ main.c
@@ -81,7 +81,6 @@ int opening(); //수정됨 게임 시작시 화면 띄우기
 void clrscr(); //수정됨 화면 지우고 (1,1)로 커서 이동
 void gotoxy(int x, int y); // 수정됨 화면 그대로 (x,y)로 이동
 void beepsound(int sel);
-void freeStage(int s);
 void freeMap();
 
 //delay함수 윈도우,리눅스용 분기 나눔<새로 추가한 함수>
@@ -195,11 +194,20 @@ int main() {
         }
         
         if (map[stage][player_y][player_x] == 'E') {
-
+            // stage++;
             score += 100;
-
+            /*
+        if (map[stage][player_y][player_x] == 'E') {
+            stage++;
+            score += 100;
+            if (stage < MAX_STAGES) {
+                init_stage();
+            } else {
+                game_over = 1;
+                game_clear();
+            }
+            */
             if (stage + 1 < stageCount) {
-                freeStage(stage);
                 stage++;
                 init_stage();
                 int re = game_clear1(); // 첫 스테이지 클리어 메시지
@@ -299,8 +307,38 @@ void beepsound(int sel) {
     }
 #endif
 }
+/*
+윈도우 버전 window.h 필요
+void beepsound(int sel){
+    switch(sel){
+        case 1:
+        Beep(900, 150);
+        delay(30);
+        Beep(700, 150);
+        delay(30);
+        Beep(500, 150);
+        delay(30);
+        Beep(350, 200);
+        delay(30);
+        break;
 
+        case 2:
+        Beep(900, 200);
+        delay(30);
+        Beep(1200, 200);
+        break;
 
+        case 3:
+        Beep(1300, 150);
+        delay(30);
+        Beep(1600, 150);
+        delay(150);
+        Beep(2000, 200);
+        delay(30);
+        break;
+}
+
+*/
 
 
 
@@ -404,7 +442,14 @@ void allocateMap(void) {
         map[s] = (char **)malloc(mapHeight[s] * sizeof(char *));
         if (!map[s]) {
             perror("map[s]쪽 malloc 실패");
-            freeMap();
+            // 이전에 할당한 이전 스테이지 메모리 해제
+            for (int i = 0; i < s; i++) {
+                for (int y = 0; y < mapHeight[i]; y++) {
+                    free(map[i][y]);
+                }
+                free(map[i]);
+            }
+            free(map);
             exit(1);
         }
 
@@ -412,7 +457,18 @@ void allocateMap(void) {
             map[s][y] = (char *)malloc(mapWidth[s] + 1);
             if (!map[s][y]) {
                 perror("map[s][y]쪽 malloc 실패");
-                freeMap();
+                // 이전에 할당한 행 해제
+                for (int yy = 0; yy < y; yy++) {
+                    free(map[s][yy]);
+                }
+                // 이전 스테이지 전체 메모리 해제
+                for (int i = 0; i < s; i++) {
+                    for (int yy = 0; yy < mapHeight[i]; yy++) {
+                        free(map[i][yy]);
+                    }
+                    free(map[i]);
+                }
+                free(map);
                 exit(1);
             }
 
@@ -449,30 +505,15 @@ void fillMap(FILE *file) {
     }
 }
 
-// 스테이지별 해제
-void freeStage(int s) {
-    if (!map[s]) return;
-
-    for (int y = 0; y < mapHeight[s]; y++) {
-        free(map[s][y]);
-    }
-    free(map[s]);
-    map[s] = NULL;
-}
-
-// 전체해제
-void freeMap() {
-    if (!map) return;
-
-    
+void freeMap(void) {
     for (int s = 0; s < stageCount; s++) {
-        if (map[s] != NULL) {
-            freeStage(s);
+        for (int y = 0; y < mapHeight[s]; y++) {
+            free(map[s][y]);
         }
+        free(map[s]);
     }
-
-    free(map);    
-    map = NULL;
+        free(map);  
+        map = NULL;
 }
 
 
